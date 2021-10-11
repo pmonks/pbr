ff364396cc0026e6888b80ea6ec842f79809081d
Peter Monks <pmonks@gmail.com>
2021-10-11 16:36:38 -0700

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+[![Build](https://github.com/pmonks/pbr/workflows/build/badge.svg?branch=main)](https://github.com/pmonks/pbr/actions?query=workflow%3Abuild) [![Lint](https://github.com/pmonks/pbr/workflows/lint/badge.svg?branch=main)](https://github.com/pmonks/pbr/actions?query=workflow%3Alint) [![Dependencies](https://github.com/pmonks/pbr/workflows/dependencies/badge.svg?branch=main)](https://github.com/pmonks/pbr/actions?query=workflow%3Adependencies) [![Open Issues](https://img.shields.io/github/issues/pmonks/discljord-utils.svg)](https://github.com/pmonks/pbr/issues) [![License](https://img.shields.io/github/license/pmonks/discljord-utils.svg)](https://github.com/pmonks/pbr/blob/main/LICENSE)
+
+
+<img alt="Ice cold can of hangover-inducing rubbish beer" align="right" width="50%" src="https://pabstblueribbon.com/wp-content/uploads/2020/10/pbr-org.png">
+
+# PBR - Peter's Build Resources
+
+A little library that extends Sean Corfield's [`build-clj`](https://github.com/IGJoshua/discljord) build tool library with:
+
+1. Convenience functions for working with the command line
+2. Additional build tasks
+
+These can be used independently; use of the convenience functions does not require use of the build tasks, and vice versa.
+
+## Features
+
+### API Documentation
+
+Coming soon.  For now, best to [browse the source](https://github.com/pmonks/pbr/tree/main/src) and/or make liberal use of the `doc` fn at the REPL.
+
+### Convenience functions 
+
+1. `exec` - more convenient / opinionated version of tools.build's [`process` function](https://clojure.github.io/tools.build/clojure.tools.build.api.html#var-process).
+2. `ensure-command` - ensure that a binary exists for the given command (note: POSIX only).
+3. `git` - easily invoke a git command.
+
+### Build tasks
+
+1. `deploy-info` - generate an EDN file containing deployment info for your code (build date/time and git commit & tag).
+2. `pom` - generate a comprehensive `pom.xml` file from EDN (which can come from anywhere - stored in your `deps.edn` or a separate file, or synthesised on the fly in your build tool script).
+3. `release` (coming soon) - perform a release by tagging the current code and creating a PR from `dev` branch to `main`. This is highly specific to the author's GitHub workflow and may not be useful for anyone else.
+
+## Using the library
+
+Express a git dependency in your `deps.edn`:
+
+```edn
+{:deps {org.github.pmonks/pbr {:git/url "https://github.com/pmonks/pbr.git"
+                               :git/sha "LATEST_GIT_SHA"}}}   ; Note: best to use the latest SHA until such time as this is deployed to Clojars
+```
+
+### Requiring the namespace
+
+In your namespace(s):
+
+```clojure
+(ns your.namespace
+  (:require [org.pmonks.pbr :as pbr]))
+```
+
+Require either or both of the included namespaces at the REPL:
+
+```clojure
+(require '[org.pmonks.pbr :as pbr])
+```
+
+## Why "PBR"?
+
+Because this code is cheap and nasty, and will give you a headache if you consume too much of it.
+
+## Contributor Information
+
+[Contributing Guidelines](https://github.com/pmonks/pbr/blob/main/.github/CONTRIBUTING.md)
+
+[Bug Tracker](https://github.com/pmonks/pbr/issues)
+
+[Code of Conduct](https://github.com/pmonks/pbr/blob/main/.github/CODE_OF_CONDUCT.md)
+
+## License
+
+Copyright © 2021 Peter Monks
+
+Distributed under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
+
+SPDX-License-Identifier: [Apache-2.0](https://spdx.org/licenses/Apache-2.0)
---
+++ src/org/pmonks/pbr.clj
@@ -0,0 +1,149 @@
+;
+; Copyright © 2021 Peter Monks
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+;
+; SPDX-License-Identifier: Apache-2.0
+;
+
+(ns org.pmonks.pbr
+  "Personal Build Resources.
+
+  The following high-level defaults are provided:
+
+  :target    \"target\",
+  :basis     (create-basis {:project \"deps.edn\"},
+  :class-dir (str target \"/classes\"),
+  :jar-file  (format \"%s/%s-%s.jar\" target lib version),
+  :uber-file (format \"%s/%s-%s.jar\" target lib version)
+             or, if :version is not provided:
+             (format \"%s/%s-standalone.jar\" target lib)
+
+  You are expected to provide :lib and :version as needed.
+
+  The following convenience fns are provided:
+
+  exec           -- req: string or [strings]
+  ensure-command -- req: command-name
+  git            -- opt: arguments to git
+
+  The following build task functions are provided, with the
+  specified required and optional hash map options:
+
+  deploy-info    -- opt :deploy-info-file (defaults to \"resources.deploy-info.edn\")
+  pom            -- req :lib, :version, :pom (see docstring for details)
+                    opt <options from clojure.tools.build.api/write-pom: https://clojure.github.io/tools.build/clojure.tools.build.api.html#var-write-pom>
+  release        -- req :version, :lib or :uber-file
+
+  All of the above return the opts hash map they were passed
+  (unlike some of the functions in clojure.tools.build.api)."
+  (:require [clojure.string          :as s]
+            [clojure.java.io         :as io]
+            [clojure.pprint          :as pp]
+            [clojure.data.xml        :as xml]
+            [clojure.tools.build.api :as b]
+            [camel-snake-kebab.core  :as csk]
+            [org.corfield.build      :as bb]))
+
+; Lame... 🙄
+(defmethod print-method java.time.Instant [^java.time.Instant inst writer]
+  (print-method (java.util.Date/from inst) writer))
+
+; ---------- CONVENIENCE FUNCTIONS ----------
+
+(defmulti exec
+  "Executes the given command line, expressed as either a string or a sequential (vector or list), optionally with other clojure.tools.build.api/process options as a second argument.
+
+  Throws ex-info on non-zero status code."
+  (fn [& args] (sequential? (first args))))
+
+(defmethod exec true
+  ([command-line] (exec command-line nil))
+  ([command-line opts]
+    (let [result (b/process (into {:command-args command-line} opts))]
+      (if (not= 0 (:exit result))
+        (throw (ex-info (str "Command '" (s/join " " command-line) "' failed (" (:exit result) ").") result))
+        result))))
+
+(defmethod exec false
+  ([command-line] (exec command-line nil))
+  ([command-line opts]
+    (exec (s/split command-line #"\s+") opts)))
+
+(defn ensure-command
+  "Ensures that the given command is available (note: POSIX only)."
+  [command]
+  (try
+    (exec ["command" "-v" command] {:out :capture :err :capture})
+    (catch clojure.lang.ExceptionInfo _
+      (throw (ex-info (str "Command " command " was not found.") {})))))
+
+(defn git
+  "Execute git with the given args, capturing and returning the output."
+  [& args]
+  (s/trim (:out (exec (concat ["git"] args) {:out :capture}))))
+
+
+; ---------- BUILD TASK FUNCTIONS ----------
+
+(defn deploy-info
+  "Writes out a deploy-info EDN file, containing at least :hash and :date keys, and possibly also a :tag key.  opts may include a :deploy-info-file key (defaults to \"resources.deploy-info.edn\")"
+  [opts]
+  (let [file-name   (get opts :deploy-info-file "resources/deploy-info.edn")
+        deploy-info (into {:hash (git "show" "-s" "--format=%H")
+                           :date (java.time.Instant/now)}
+                          (try {:tag (git "describe" "--tags" "--exact-match")} (catch clojure.lang.ExceptionInfo _ nil)))]
+    (io/make-parents file-name)
+    (with-open [w (io/writer (io/file file-name))]
+      (pp/pprint deploy-info w))))
+
+(defn- pom-keyword
+  "Converts a regular Clojure keyword into a POM-compatible keyword."
+  [kw]
+  (keyword "xmlns.http%3A%2F%2Fmaven.apache.org%2FPOM%2F4.0.0" (csk/->camelCase (name kw))))
+
+(defn- build-pom
+  "Converts the given 'element' into a POM structure in clojure.data.xml compatible EDN format."
+  [elem]
+  (cond
+    (map-entry?  elem)             [(pom-keyword (key elem)) (build-pom (val elem))]
+    (map?        elem)             (map build-pom elem)
+    (sequential? elem)             (mapcat #(hash-map (pom-keyword (first elem)) (build-pom %)) (rest elem))
+    (= java.util.Date (type elem)) (str (.toInstant ^java.util.Date elem))
+    :else                          (str elem)))
+
+(defn pom
+  "Writes out a pom file. opts may include:
+
+  :lib       -- opt: a symbol identifying your project e.g. 'org.github.pmonks/pbr
+  :version   -- opt: a string containing the version of your project e.g. \"1.0.0-SNAPSHOT\"
+  :pom-file  -- opt: the name of the file to write to (defaults to \"./pom.xml\"
+  :write-pom -- opt: a flag determining whether to invoke the tools.build `write-pom` fn after generating the pom (note: it probably doesn't do what you're expecting...)
+  :pom       -- opt: a map containing other POM elements (see https://maven.apache.org/pom.html for details)."
+  [opts]
+  (let [pom-file (get opts :pom-file "./pom.xml")
+        pom-in   (merge (when (:lib     opts) {:group-id (namespace (:lib opts)) :artifact-id (name (:lib opts)) :name (name (:lib opts))})
+                        (when (:version opts) {:version (:version opts)})
+                        (:pom opts))   ; Merge user-supplied values last, so that they always take precedence
+        pom-out  [(pom-keyword :project) {:xmlns                         "http://maven.apache.org/POM/4.0.0"
+                                         (keyword "xmlns:xsi")          "http://www.w3.org/2001/XMLSchema-instance"
+                                         (keyword "xsi:schemaLocation") "http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"}
+                   (concat [[(pom-keyword :model-version) "4.0.0"]]
+                            (build-pom pom-in))]
+        pom-xml  (xml/sexp-as-element pom-out)]
+    (with-open [pom-writer (io/writer pom-file)]
+      (xml/emit pom-xml pom-writer :encoding "UTF8"))
+    (when (:write-pom opts)
+      (b/write-pom (merge (assoc opts :src-pom pom-file)
+                          (when-not (:basis     opts) {:basis     (bb/default-basis)})
+                          (when-not (:class-dir opts) {:class-dir (bb/default-class-dir)}))))))
