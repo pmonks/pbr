3505706358e97aceda852014ecca05ac1ce820a9
Peter Monks <pmonks@gmail.com>
2023-02-27 11:51:47 -0800

    :new: Improve JAR version numbering

--- pbr.clj
+++ pbr.clj
@@ -21,7 +21,7 @@
   [opts]
   (assoc opts
          :lib          'com.github.pmonks/pbr
-         :version      (pbr/default-version)
+         :version      (pbr/calculate-version 2 0)
          :write-pom    true
          :validate-pom true
          :pom          {:description      "Peter's Build Resources for Clojure tools.build projects."
