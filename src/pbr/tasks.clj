0a7e25dd9cb15d53bfe4cd307c8cb19d7d6ff412
Peter Monks <pmonks@gmail.com>
2023-09-23 21:36:31 -0700

    :ambulance: Fix slf4j warning from antq

--- src/pbr/tasks.clj
+++ src/pbr/tasks.clj
@@ -183,7 +183,7 @@
   [opts]
   (println "ℹ️ Checking for outdated dependencies...")
   (tc/clojure "-Sdeps"
-              (str "{:aliases {:antq {:extra-deps {com.github.liquidz/antq " (pr-str ver-antq) "} :main-opts [\"-m\" \"antq.core\"]}}}")
+              (str "{:aliases {:antq {:extra-deps {com.github.liquidz/antq " (pr-str ver-antq) " ch.qos.logback/logback-classic " (pr-str ver-logback) "} :main-opts [\"-m\" \"antq.core\"]}}}")
               "-M:antq"
               "--ignore-locals"
               "--skip=pom")
