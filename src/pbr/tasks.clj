33a91871900a02c90d1a4e3acb7575ecc9e9edfa
Peter Monks <pmonks@gmail.com>
2024-06-14 15:14:21 -0700

    :arrow_up: Upgrade dependencies

--- src/pbr/tasks.clj
+++ src/pbr/tasks.clj
@@ -32,7 +32,7 @@
 
 (def ^:private ver-clj-check   {:git/sha "d997df866b2a04b7ce7b17533093ee0a2e2cb729"})
 (def ^:private ver-test-runner {:git/tag "v0.5.1" :git/sha "dfb30dd"})
-(def ^:private ver-slf4j       {:mvn/version "2.0.10"})
+(def ^:private ver-slf4j       {:mvn/version "2.0.13"})
 (def ^:private ver-log4j2      {:mvn/version "2.23.1"})
 (def ^:private ver-eastwood    {:mvn/version "1.4.0"})
 (def ^:private ver-codox       {:mvn/version "0.10.8"})
