a88439776462b8fe23b736a922b949f10d3089e1
Peter Monks <pmonks@gmail.com>
2024-07-11 13:04:03 -0700

    :ambulance: Fix infinite loop

--- src/pbr/build.clj
+++ src/pbr/build.clj
@@ -103,7 +103,7 @@ clojure -A:deps -T:build help/doc"
   "Run the CI pipeline."
   [opts]
   (-> opts
-      ci))
+      pbr/ci))
 
 ; We do these cursed shenanigans because tools-licenses requires JDK 11+, but we want PBR to work on JDK 8+
 (if (>= (pbr/jvm-version) 11)
